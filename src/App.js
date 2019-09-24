5cc1aaaf5030f97e56b52d36d29872888a8c58ca
DavidGOD228 <god.of.cplpl@gmail.com>
2019-09-24 20:08:02 +0300

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faBars,
   faChevronRight,
   faChevronLeft
-} from "@fortawesome/free-solid-svg-icons";
+} from '@fortawesome/free-solid-svg-icons';
 
-import Sidebar from "./Layout/Sidebar";
-import PhotosGallery from "./Layout/PhotosGallery";
+import Sidebar from './Layout/Sidebar';
+import PhotosGallery from './Layout/PhotosGallery';
 // import MyMap from "./Layout/Map";
-import MyMap from "./Layout/Map1";
-import VariablesBlock from "./VariablesBlock";
+import MyMap from './Layout/ModernMap';
+import VariablesBlock from './VariablesBlock';
 
-import "./index.css";
+import './index.css';
 
 class App extends Component {
   constructor(props) {
@@ -45,27 +45,72 @@ class App extends Component {
     return (
       <BrowserRouter>
         <div className="min-h-screen md:flex">
-          <div className={`flex flex-col content-around flex-none bg-gray-800 text-white relative ${ this.state.sidebarOn ? "w-full md:max-w-xs" : "w-0"}`}>
+          <div
+            className={`flex flex-col content-around flex-none bg-gray-800 text-white relative ${
+              this.state.sidebarOn ? 'w-full md:max-w-xs' : 'w-0'
+            }`}
+          >
             <Sidebar>
-              <div className={`toggle-button cursor-pointer`} onClick={() => this.toggleSidebar(this)}>
-                <FontAwesomeIcon className="mr-2" icon={this.state.sidebarOn ? faChevronLeft : faChevronRight} />
+              <div
+                className={`toggle-button cursor-pointer`}
+                onClick={() => this.toggleSidebar(this)}
+              >
+                <FontAwesomeIcon
+                  className="mr-2"
+                  icon={this.state.sidebarOn ? faChevronLeft : faChevronRight}
+                />
               </div>
 
-              <div className={`menu-container ${this.state.menuOn ? "" : "h-0"}`}>
-                <Link to="/map" className="menu-item flex flex-col justify-start items-center content-center text-center w-full bg-blue-700 py-2 cursor-pointer">Map</Link>
-                <Link to="/" className="menu-item flex flex-col justify-start items-center content-center text-center w-full bg-blue-700 py-2 cursor-pointer">Load photos</Link>
+              <div
+                className={`menu-container ${this.state.menuOn ? '' : 'h-0'}`}
+              >
+                <Link
+                  to="/map"
+                  className="menu-item flex flex-col justify-start items-center content-center text-center w-full bg-blue-700 py-2 cursor-pointer"
+                >
+                  Map
+                </Link>
+                <Link
+                  to="/"
+                  className="menu-item flex flex-col justify-start items-center content-center text-center w-full bg-blue-700 py-2 cursor-pointer"
+                >
+                  Load photos
+                </Link>
               </div>
 
-              <div className="flex flex-col justify-start items-center content-center text-center w-full bg-blue-700 py-2 cursor-pointer" onClick={() => this.toggleMenu(this)}>
+              <div
+                className="flex flex-col justify-start items-center content-center text-center w-full bg-blue-700 py-2 cursor-pointer"
+                onClick={() => this.toggleMenu(this)}
+              >
                 <FontAwesomeIcon className="mr-2" icon={faBars} />
               </div>
 
               <VariablesBlock
                 vars={[
-                  { name: "Duration", val: "8.60", icon: "coffee", measuredIn: "minutes" },
-                  { name: "Resolution", val: 1.7, icon: "coffee", measuredIn: "in/px" },
-                  { name: "Area", val: 49, icon: "coffee", measuredIn: "acres" },
-                  { name: "Battery", val: 3900, icon: "coffee", measuredIn: "mph"}
+                  {
+                    name: 'Duration',
+                    val: '8.60',
+                    icon: 'coffee',
+                    measuredIn: 'minutes'
+                  },
+                  {
+                    name: 'Resolution',
+                    val: 1.7,
+                    icon: 'coffee',
+                    measuredIn: 'in/px'
+                  },
+                  {
+                    name: 'Area',
+                    val: 49,
+                    icon: 'coffee',
+                    measuredIn: 'acres'
+                  },
+                  {
+                    name: 'Battery',
+                    val: 3900,
+                    icon: 'coffee',
+                    measuredIn: 'mph'
+                  }
                 ]}
               />
             </Sidebar>

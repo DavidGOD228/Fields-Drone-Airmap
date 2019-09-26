5810b95359c2ce12ae374645534ada91153e958a
Stas <Stas@DESKTOP-548ELRA>
2019-09-26 17:46:04 +0300

    map updated

--- src/Layout/MyMapTest.js
+++ src/Layout/MyMapTest.js
@@ -4,6 +4,19 @@ class MyMap extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      photos: [],
+
+      base: null,
+      field: null,
+      readyForStart: false,
+
+      currentLabelIdx: 0,
+      labels: [
+        "Set the base",
+        "Set the field"
+      ],
+      
+      map: null,
       drawingManager: null,
       selectedShape: null,
       colors: ["#1E90FF", "#FF1493", "#32CD32", "#FF8C00", "#4B0082"],
@@ -17,6 +30,7 @@ class MyMap extends Component {
     const googleMapScript = document.createElement("script");
     googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyBkDqO4ZFc9wLSfg-6qHo5xdAGusxTsRyI&libraries=drawing`;
     window.document.body.appendChild(googleMapScript);
+
     googleMapScript.addEventListener("load", () => {
       this.initialize();
     });
@@ -35,7 +49,8 @@ class MyMap extends Component {
 
   setSelection(shape) {
     if (shape.type !== "marker") {
-      this.clearSelection();
+      this.clearSelection.call(this);
+
       shape.setEditable(true);
       this.selectColor(shape.get("fillColor") || shape.get("strokeColor"));
     }
@@ -56,18 +71,7 @@ class MyMap extends Component {
       selectedColor: color
     });
 
-    const bufButtonsArr = this.state.colorButtons;
-    for (var i = 0; i < this.state.colors.length; ++i) {
-      var currColor = this.state.colors[i];
-      bufButtonsArr[currColor].style.border =
-        currColor == color ? "2px solid #789" : "2px solid #fff";
-    }
-    
-    this.setState({
-      colorButtons: bufButtonsArr
-    })
-
-    let polygonOptions = this.state.this.state.drawingManager.get("polygonOptions");
+    let polygonOptions = this.state.drawingManager.get("polygonOptions");
     polygonOptions.fillColor = color;
     this.state.drawingManager.set("polygonOptions", polygonOptions);
   }
@@ -96,28 +100,12 @@ class MyMap extends Component {
     return button;
   }
 
-  buildColorPalette() {
-    var colorPalette = document.getElementById("color-palette");
-    const bufArr = this.state.colorButtons;
-    for (var i = 0; i < this.state.colors.length; ++i) {
-      var currColor = this.state.colors[i];
-      var colorButton = this.makeColorButton(currColor);
-      colorPalette.appendChild(colorButton);
-      bufArr[currColor] = colorButton;
-    }
-    this.setState({
-      colorButtons: bufArr
-    })
-    this.selectColor(this.state.colors[0]);
-  }
-
   initialize() {
-    
-    var map = new window.google.maps.Map(this.googleMapRef.current, {
+    let map = new window.google.maps.Map(this.googleMapRef.current, {
       zoom: 16,
       center: {
-        lat: 43.642567,
-        lng: -79.387054
+        lat: 51.359313,
+        lng: 25.499893
       },
       mapTypeId: window.google.maps.MapTypeId.SATELLITE,
       disableDefaultUI: true,
@@ -130,42 +118,112 @@ class MyMap extends Component {
       editable: true,
       draggable: true
     };
+
+    let that = this;
+
     let drawingManager = new window.google.maps.drawing.DrawingManager({
-      drawingMode: window.google.maps.drawing.OverlayType.POLYGON,
+      drawingMode: window.google.maps.drawing.OverlayType.MARKER,
       drawingControlOptions: {
         position: window.google.maps.ControlPosition.TOP_CENTER,
         drawingModes: ['marker', 'polygon']
       },
-      markerOptions: {
-        draggable: false
-      },
+      
       polygonOptions: polyOptions,
       markerOptions: {
+        draggable: true,
         icon:
           'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
       }
     });
     drawingManager.setMap(map);
+
+    window.google.maps.event.addListener(drawingManager, 'polygoncomplete', (polygon) => {
+      const polyArr = []
+      for (var i = 0; i < polygon.getPath().getLength(); i++) {
+        polyArr.push({
+          lat: polygon.getPath().getAt(i).lat(),
+          lng: polygon.getPath().getAt(i).lng()  
+        });
+      }
+      console.log('polyArr :', polyArr);
+      // FIXME: not customizable at all 
+
+
+      this.setState({
+        field: polyArr
+      })
+
+      if(!this.state.labels[this.state.currentLabelIdx + 1]) { 
+        this.stopDrawing();
+        this.setState({
+          readyForStart: true
+        })
+      }
+      this.setState(state => {
+        return {
+          currentLabelIdx: state.currentLabelIdx + 1,
+        }
+      })
+
+      // this.stopDrawing();
+    });
+
+    window.google.maps.event.addListener(drawingManager, 'markercomplete', (marker) => {
+      const pos = {
+        lat: marker.getPosition().lat(),
+        lng: marker.getPosition().lng()
+      }
+      console.log('homeMarker.getPosition():', pos);
+
+      this.setState({
+        base: pos
+      })
     
-    this.setState({
-      drawingManager: drawingManager,
-      map: map
-    })
+      if(!this.state.labels[this.state.currentLabelIdx + 1]) { 
+        this.stopDrawing();
+        this.setState({
+          readyForStart: true
+        })
+      }
+      this.setState(state => {
+        return {
+          currentLabelIdx: state.currentLabelIdx + 1
+        }
+      })
+
+      this.state.drawingManager.setDrawingMode(window.google.maps.drawing.OverlayType.POLYGON);
+
+      this.createDrone({
+        position: pos,
+        map: this.state.map,
+        icon: {
+          url: "https://image.flaticon.com/icons/svg/215/215736.svg",
+          scaledSize: new window.google.maps.Size(32, 32)
+        }
+      })
+    
+
+      // FIXME: not customizable at all 
+    });
+
+    // TODO: Delete map element
+    // window.google.maps.event.addListener(drawingManager, 'overlaycomplete', (event) => {
+    //   var element = event.overlay;
+    //   window.google.maps.event.addListener(element, 'click', (e) => {
+    //     element.setMap(null);
+    //   });
+    // });
 
     window.google.maps.event.addListener(
-      this.state.drawingManager,
+      drawingManager,
       "overlaycomplete",
-      function(e) {
+      (e) => {
         var newShape = e.overlay;
 
         newShape.type = e.type;
 
         if (e.type !== window.google.maps.drawing.OverlayType.MARKER) {
-          // Switch back to non-drawing mode after drawing a shape.
-          this.state.drawingManager.setDrawingMode(null);
-
-          // Add an event listener that selects the newly-drawn shape when the user
-          // mouses down on it.
+          drawingManager.setDrawingMode(null);
           window.google.maps.event.addListener(newShape, "click", function(e) {
             if (e.vertex !== undefined) {
               if (
@@ -190,40 +248,69 @@ class MyMap extends Component {
       }
     );
 
-  
     window.google.maps.event.addListener(
-      this.state.drawingManager,
+      drawingManager,
       "drawingmode_changed",
-      this.clearSelection
-    );
-    window.google.maps.event.addListener(map, "click", this.clearSelection);
-    window.google.maps.event.addDomListener(
-      document.getElementById("delete-button"),
-      "click",
-      this.deleteSelectedShape
+      () => this.clearSelection.call(that)
     );
 
-    this.buildColorPalette();
+    window.google.maps.event.addListener(map, "click", () => this.clearSelection.call(that));
+
+    this.setState({
+      drawingManager: drawingManager,
+      map: map
+    })
   }
 
-  createGoogleMap() {
-    return new window.google.maps.Map(this.googleMapRef.current, {
-      zoom: 16,
-      center: {
-        lat: 43.642567,
-        lng: -79.387054
-      },
-      disableDefaultUI: true
-    });
+  stopDrawing() {
+    this.state.drawingManager.setMap(null);
   }
+
+  addLatLngToPoly(latLng, poly) {
+    var path = poly.getPath();
+    path.push(latLng);
+    var encodeString = window.google.maps.geometry.encoding.encodePath(path);
+    console.log('encodeString :', encodeString);
+    // if (encodeString) {
+    //   document.getElementById('encoded-polyline').value = encodeString;
+    // }
+  }
+
+  createDrone(options, place) {
+    let marker = new window.google.maps.Marker(options);
+    // marker.place_id = place.id;
+    // markers[place.id] = marker;
+    // let infowindow = new window.google.maps.InfoWindow({
+    //   content: place.details
+    // });
+  
+    // infowindows[place.id] = infowindow;
+  
+    // window.google.maps.event.addListener(marker, 'click', function() {
+    //   // infowindows[marker.place_id].open(map,marker);
+    // });
+  
+   }
 
   render() {
-    // const { labels, currentLabelIdx } = this.state;
-    // console.log("labels :", labels);
-    // console.log("currentLabelIdx :", currentLabelIdx);
+    const { labels, currentLabelIdx, readyForStart } = this.state;
+    console.log('readyForStart :', readyForStart);
     return (
       <div className="relative">
-        {/* <div id="help-container">{labels[currentLabelIdx]}</div> */}
+        <div 
+          id="help-container" 
+          style={{display: labels[currentLabelIdx] ? 'flex' : 'none' }}
+        >
+          {labels[currentLabelIdx]}
+        </div>
+
+        <button 
+          className="start-flight-button click-scale-down bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+          style={{display: readyForStart ? "block" : "none" }}
+        >
+          Start
+        </button>
+
         <div
           ref={this.googleMapRef}
           id="google-map"

7dafea53c4f84e7d6471e63a4752b0f5081c24d9
Stas <Stas@DESKTOP-548ELRA>
2019-10-12 14:42:07 +0300

    blur done

--- src/calculations/Photo.js
+++ src/calculations/Photo.js
@@ -5,6 +5,7 @@ const jimp = window.require('jimp');
 const fs = window.require('fs');
 const http = window.require('http');
 const https = window.require('https')
+const path = window.require('path')
 
 // console.log('mergeImg :', mergeImg);
 
@@ -12,7 +13,7 @@ const https = window.require('https')
 
 // console.log("fs :", fs);
 let se = sightengine("540865617", "38b6kZYxVz6DyZLGv82G");
-console.log("sightengine :", se);
+console.log("se :", se);
 
 class Photo {
   constructor({ url }, additional) {
@@ -70,35 +71,62 @@ class Photo {
   }
 
   static blur(inputFilepath, outputFilepath, blurFactor = 20) {
-    // fs.readFile(inputFilepath, function(err, file) {
-    //   if (err) throw err; // Fail if the file can't be read.
-    
-    //   const image = URL.createObjectURL(new Blob([file], {type: "image/jpg"})); 
-
-    //   image.blur(blurFactor, function(err){ 
-    //     if (err) throw err; 
-    //   }).write(outputFilepath);   
-    // });    
+    jimp.read(inputFilepath)
+      .then(file => {
+        return file
+          .blur(blurFactor)
+          .write(outputFilepath); // save
+      })
+      .catch(err => {
+        console.error(err);
+      });
   }
 
-  // static async getUrlBlur(url) {
-  //   return await sightengine
-  //     .check(["properties"])
-  //     .set_url(
-  //       "https://sightengine.com/assets/img/examples/example-prop-c1.jpg"
-  //     )
-  //     .then(function(result) {
-  //       console.log("RESULT IMAGE :", result);
-  //     })
-  //     .catch(function(err) {
-  //       // Handle error
-  //     });
-  // }
+  static async getUrlBlurFactor(url) {
+    return await se
+      .check(["properties"])
+      .set_url(
+        "https://sightengine.com/assets/img/examples/example-prop-c1.jpg"
+      )
+      // .then(function(result) {
+      //   console.log("RESULT IMAGE :", result);
+      // })
+      // .catch(function(err) {
+      //   // Handle error
+      // });
+  }
+
+  static getFileBlurFactor(filepath) {
+    console.log("file blur");
+    return  se
+      .check(['properties'])
+      .set_file(filepath)
+      .then(function(result) {
+        console.log('FILE BLUR :', result);
+      }).catch(function(err) {
+        // Error
+      });
+  }
 }
 
-// Photo.merge();
-Photo.blurUrl('https://images.unsplash.com/photo-1531804055935-76f44d7c3621?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80', "BlurKek.jpg");
+Photo.merge();
+// Photo.blurUrl('https://images.unsplash.com/photo-1531804055935-76f44d7c3621?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80', "BlurKek.jpg");
 // Photo.blur("./Photos/Flight29/0.jpg", "./0.jpg", 20)
+// Photo.getFileBlurFactor('./0.jpg')
+Photo.getUrlBlurFactor().then(res => console.log("url sharpness: ", res.sharpness)); 
+// Photo.getFileBlurFactor("./Photos/Flight29/0.jpg")
+console.log("sd");
+// Photo.getFileBlurFactor("0.jpg")
+
+// var binaryImage = fs.createReadStream(path.resolve(__dirname,'0.jpg'));
+ 
+// se.check(['nudity', 'type', 'properties','wad','faces', 'celebrities']).set_bytes(binaryImage).then(function(result) {
+//   // The result of the API
+//   console.log('result :', result);
+// }).catch(function(err) {
+//   // Error
+// });
+// .then(res => console.log("url sharpness: ", res.sharpness));
 
 // Photo.downloadUrl("ThIRD_PHOTO.jpg", "http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg")
 

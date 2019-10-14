5e12527812d23e6f2af4fde7f430d331ac88e5d2
DavidGOD228 <god.of.cplpl@gmail.com>
2019-10-14 20:53:27 +0300

    color invercion

--- src/calculations/Photo.js
+++ src/calculations/Photo.js
@@ -1,7 +1,7 @@
-import sightengine from "sightengine";
+import sightengine from 'sightengine';
 
 const mergeImg = window.require('merge-img');
-const mergeImages = window.require('merge-images')
+const mergeImages = window.require('merge-images');
 const Jimp = window.require('jimp');
 
 const fs = window.require('fs');
@@ -15,9 +15,8 @@ const path = window.require('path');
 // downloadUrlToFile("Second_PHOTO.jpg", "http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg");
 
 // console.log("fs :", fs);
-let se = sightengine("540865617", "38b6kZYxVz6DyZLGv82G");
-console.log("se :", se);
-
+let se = sightengine('540865617', '38b6kZYxVz6DyZLGv82G');
+console.log('se :', se);
 
 class Photo {
   constructor({ url }, additional) {
@@ -30,9 +29,7 @@ class Photo {
     this.url = url;
   }
 
-  
-
-  static downloadUrl(filename, url) { 
+  static downloadUrl(filename, url) {
     const file = fs.createWriteStream(filename);
     const request = https.get(url, function(response) {
       response.pipe(file);
@@ -48,36 +45,36 @@ class Photo {
 
   // async static blur() {
   static async blurUrl(url, outputFilepath, blurFactor = 20) {
-    // const image = await Jimp.read 
-    const image = await Jimp.read(url); 
+    // const image = await Jimp.read
+    const image = await Jimp.read(url);
     console.log('image :', image);
 
-    image.blur(blurFactor, function(err){ 
-      if (err) throw err; 
-    }).write(outputFilepath);   
+    image
+      .blur(blurFactor, function(err) {
+        if (err) throw err;
+      })
+      .write(outputFilepath);
   }
 
   static blur(inputFilepath, outputFilepath, blurFactor = 20) {
     Jimp.read(inputFilepath)
       .then(file => {
-        return file
-          .blur(blurFactor)
-          .write(outputFilepath); // save
+        return file.blur(blurFactor).write(outputFilepath); // save
       })
       .catch(err => {
         console.error(err);
       });
   }
 
-
   static getFileBlurFactor(filepath) {
-    console.log("file blur");
-    return  se
+    console.log('file blur');
+    return se
       .check(['properties'])
       .set_file(filepath)
       .then(function(result) {
         console.log('FILE BLUR :', result);
-      }).catch(function(err) {
+      })
+      .catch(function(err) {
         // Error
       });
   }
@@ -115,7 +112,7 @@ class Photo {
   //     jimps.push(j);
   //   }
   //   console.log('jimps :', jimps);
-    
+
   //   var mainImage = await new Jimp(1200, 1200, 0x0);
 
   //   mainImage.composite(jimps[0], 0, 0);
@@ -125,9 +122,9 @@ class Photo {
   // }
 
   static async compositeImages(canvas, img) {
-    console.log("CALLED CI");
+    console.log('CALLED CI');
     let j = await Jimp.read(img.src);
-    canvas.composite(j, img.x, img.y);    
+    canvas.composite(j, img.x, img.y);
     return canvas;
   }
 
@@ -138,25 +135,35 @@ class Photo {
   //   console.log('img.src :', "./Photos/Flight89/1.jpg");
   //   let j = await Jimp.read("./Photos/Flight89/1.jpg");
   //   console.log('canvas, j :', canvas, j);
-  //   canvas.composite(j, img.x, img.y);    
+  //   canvas.composite(j, img.x, img.y);
   //   canvas.write(outputPath, () => console.log('DONE COMPOSING'))
   //   return canvas;
   // }
-
+  static async inverceColor(canvas) {
+    canvas.flip(false, true);
+    canvas.invert();
+    canvas.color([
+      { apply: 'red', params: [100] },
+      { apply: 'green', params: [-100] },
+      { apply: 'blue', params: [-100] }
+    ]);
+    canvas.brightness(-0.4);
+    canvas.contrast(0.3);
+  }
   static async compositeImagesAndSave(mainImg, imgs, outputPath) {
-    console.log("CALLED CIAS");
+    console.log('CALLED CIAS');
     // console.log('img.src :', img.src);
     // let j = await Jimp.read(img.src);
     // console.log('img.src :', "./Photos/Flight89/1.jpg");
     let canvas = mainImg.mapImg;
     let jimps = [];
 
-    // canvas.contain(mainImg.nYPixels, mainImg.nXPixels, 0, Jimp.HORIZONTAL_ALIGN_LEFT | Jimp.VERTICAL_ALIGN_BOTTOM); 
+    // canvas.contain(mainImg.nYPixels, mainImg.nXPixels, 0, Jimp.HORIZONTAL_ALIGN_LEFT | Jimp.VERTICAL_ALIGN_BOTTOM);
     // FIXME: -1 is not right
-    for(let i = 0; i < imgs.length - 1; i++) {
+    for (let i = 0; i < imgs.length - 1; i++) {
       let j = await Jimp.read(imgs[i].src);
-      // console.log('idx, j :',i, j);  
-      jimps.push(j)
+      // console.log('idx, j :',i, j);
+      jimps.push(j);
     }
     // for(let [idx,img] of imgs.entries()) {
     //   let j = await Jimp.read(img.src);
@@ -165,15 +172,17 @@ class Photo {
     // }
 
     // console.log('jimps, imgs :', jimps, imgs);
-    for(let [idx, j] of jimps.entries()) {
-      canvas.composite(j, imgs[idx].x, imgs[idx].y)
+    for (let [idx, j] of jimps.entries()) {
+      canvas.composite(j, imgs[idx].x, imgs[idx].y);
     }
 
     // let j = await Jimp.read("./Photos/Flight89/1.jpg");
     // console.log('canvas, j :', canvas, j);
-    // canvas.composite(j, img.x, img.y);   
-    canvas.flip(false, true);
-    canvas.write(outputPath, () => console.log('DONE COMPOSING'))
+    // canvas.composite(j, img.x, img.y);
+
+    //this.inverceColor(canvas);
+
+    canvas.write(outputPath, () => console.log('DONE COMPOSING'));
 
     return canvas;
   }
@@ -190,7 +199,6 @@ class Photo {
 //   console.log('mapImg :', mapImg);
 // })();
 
-
 // Photo.compositeImages(
 //   [
 //     "./Photos/Flight85/1.jpg",
@@ -206,13 +214,13 @@ class Photo {
 // Photo.blurUrl('https://images.unsplash.com/photo-1531804055935-76f44d7c3621?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80', "BlurKek.jpg");
 // Photo.blur("./Photos/Flight29/0.jpg", "./0.jpg", 20)
 // Photo.getFileBlurFactor('./0.jpg')
-// Photo.getUrlBlurFactor().then(res => console.log("url sharpness: ", res.sharpness)); 
+// Photo.getUrlBlurFactor().then(res => console.log("url sharpness: ", res.sharpness));
 // Photo.getFileBlurFactor("./Photos/Flight29/0.jpg")
 // console.log("sd");
 // Photo.getFileBlurFactor("0.jpg")
 
 // var binaryImage = fs.createReadStream(path.resolve(__dirname,'0.jpg'));
- 
+
 // se.check(['nudity', 'type', 'properties','wad','faces', 'celebrities']).set_bytes(binaryImage).then(function(result) {
 //   // The result of the API
 //   console.log('result :', result);

f390976d7e65c95eab96a8e544f540d65c4b90bc
DavidGOD228 <god.of.cplpl@gmail.com>
2019-10-18 18:19:25 +0300

    My way

--- src/calculations/Photo.js
+++ src/calculations/Photo.js
@@ -1,44 +1,14 @@
-// var child_process = window.require("child_process");
-// const execFile = child_process.execFile;
+const Rembrandt = require('rembrandt/build/browser');
+const mergeImg = window.require('merge-img');
+const mergeImages = window.require('merge-images');
+const Jimp = window.require('jimp');
 
-// FIXME:
-// ./ConsoleApplication1.exe 3 1 1 1 6
-//  6 6 3 3 3 6 0.01 0.01 0.02 1000 1 1
-// 0 0 0
+const fs = window.require('fs');
+const http = window.require('http');
+const https = window.require('https');
+const path = window.require('path');
 
-// // this launches the executable and returns immediately
-// let nPoints = 3,
-//  p1 = [1, 1, 1],
-//  p2 = [1, 1, 1],
-//  p3 = [1, 1, 1],
-//
-//
-//
-// var child = execFile("ConsoleApplication1.exe", ["3", "arg2"], function(
-//   error,
-//   stdout,
-//   stderr
-// ) {
-//   // This callback is invoked once the child terminates
-//   // You'd want to check err/stderr as well!
-//   console.log("Here is the complete output of the program: ");
-//   console.log(stdout);
-// });
-
-// // if the program needs input on stdin, you can write to it immediately
-// child.stdin.setEncoding("utf-8");
-// child.stdin.write("Hello my child!\n");
-
-const mergeImg = window.require("merge-img");
-const mergeImages = window.require("merge-images");
-const Jimp = window.require("jimp");
-
-const fs = window.require("fs");
-const http = window.require("http");
-const https = window.require("https");
-const path = window.require("path");
-
-const se = window.require("sightengine")("540865617", "38b6kZYxVz6DyZLGv82G");
+const se = window.require('sightengine')('540865617', '38b6kZYxVz6DyZLGv82G');
 
 class Photo {
   constructor({ url }, additional) {
@@ -60,7 +30,7 @@ class Photo {
 
   static async blurUrl(url, outputFilepath, blurFactor = 20) {
     const image = await Jimp.read(url);
-    console.log("image :", image);
+    console.log('image :', image);
 
     image
       .blur(blurFactor, function(err) {
@@ -70,19 +40,43 @@ class Photo {
   }
 
   static async compositeImages(canvas, img) {
-    console.log("CALLED CI");
+    console.log('CALLED CI');
     let j = await Jimp.read(img.src);
     canvas.composite(j, img.x, img.y);
     return canvas;
   }
 
+  static async uploadIMG(filepathA) {
+    var url;
+    var file = fs.readFileSync(filepathA);
+    var base64 = new Buffer(file).toString('base64');
+    console.log(file, base64);
+    fetch(
+      'https://api.imgbb.com/' +
+        file.filename +
+        '/' +
+        base64 +
+        '?key=06706fe2cc8c18bdcce5f4424f86a038'
+    )
+      .then(res => res.json())
+      .then(
+        result => {
+          console.log(result);
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    return url;
+  }
+
   static async inverceColor(canvas) {
-    // canvas.flip(false, true);
+    canvas.flip(false, true);
     canvas.invert();
     canvas.color([
-      { apply: "red", params: [100] },
-      { apply: "green", params: [-100] },
-      { apply: "blue", params: [-100] }
+      { apply: 'red', params: [100] },
+      { apply: 'green', params: [-100] },
+      { apply: 'blue', params: [-100] }
     ]);
     canvas.brightness(-0.4);
     canvas.contrast(0.3);
@@ -93,7 +87,7 @@ class Photo {
     let jimps = [];
 
     // FIXME: -1 is not right
-    for (let i = 0; i < imgs.length; i++) {
+    for (let i = 0; i < imgs.length - 1; i++) {
       let j = await Jimp.read(imgs[i].src);
       jimps.push(j);
     }
@@ -102,8 +96,7 @@ class Photo {
     }
     //this.inverceColor(canvas);
 
-    // canvas.flip(true, false);
-    canvas.write(outputPath, () => console.log("DONE COMPOSING"));
+    canvas.write(outputPath, () => console.log('DONE COMPOSING'));
 
     return canvas;
   }
@@ -118,16 +111,43 @@ class Photo {
       });
   }
 
+  static async comparingImages(filepathA, filepathB) {
+    var tempA = filepathA;
+    var tempB = filepathB;
+
+    const rembrandt = new Rembrandt({
+      imageA: tempA,
+      imageB: tempB,
+      thresholdType: Rembrandt.THRESHOLD_PERCENT,
+      maxThreshold: 0.01,
+      maxDelta: 20,
+      maxOffset: 15,
+      renderComposition: true, // Should Rembrandt render a composition image?
+      compositionMaskColor: Rembrandt.Color.RED // Color of unmatched pixels
+    });
+
+    rembrandt
+      .compare()
+      .then(result => {
+        console.log('Passed:', result.passed);
+        console.log('Difference:', (result.threshold * 100).toFixed(2), '%');
+        console.log('Composition image buffer:', result.compositionImage);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
   static getFileBlurFactor(filepath) {
     return se
-      .check(["properties"])
+      .check(['properties'])
       .set_file(filepath)
       .then(function(result) {
-        console.log("result", result);
+        console.log('result', result);
         return result.sharpness;
       })
       .catch(function(err) {
-        console.log("err :", err);
+        console.log('err :', err);
       });
   }
 }
@@ -136,6 +156,6 @@ class Photo {
 //   console.log("shit: ", await Photo.getFileBlurFactor("./4out.jpg"));
 // })();
 
-Photo.getFileBlurFactor("./4out.jpg").then(res => console.log("res :", res));
+Photo.getFileBlurFactor('./4out.jpg').then(res => console.log('res :', res));
 
 export default Photo;
